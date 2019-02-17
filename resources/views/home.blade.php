1d1ee2a50c9b2e30e8ac3a5369f62efdbe7db434
Igor <igor_pavlenko@yahoo.com>
2019-02-17 12:44:53 +0100

    User\AreaController created with route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,17 @@
 <div class="container">
     <div class="jumbotron">
         <div class="row">
-            @foreach ($areas as $country )
+            @foreach ($areas as $country)
                 <div class="col-md-12">
-                    <h2><a href="">{{ $country->name }}</a></h2>
+                    <h2><a href="{{ route('user.area.store', $country) }}">{{ $country->name }}</a></h2>
                     <hr>
                     <div class="row">
                         @foreach ($country->children as $obl)
                         <div class="col-md-4">
-                            <h3><a href="">{{ $obl->name }}</a></h3>
+                            <h3><a href="{{ route('user.area.store', $obl) }}">{{ $obl->name }}</a></h3>
                                 <hr>
                                 @foreach ($obl->children as $town)
-                                    <h5><a href="">{{ $town->name }}</a></h5>
+                                    <h5><a href="{{ route('user.area.store', $town) }}">{{ $town->name }}</a></h5>
                                 @endforeach
                             </div>
                         @endforeach
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,6 @@ Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
+Route::get('/user/area/{area}', 'User\AreaController@store')->name('user.area.store'); // we use here route binding and will be passing our route slug here
+
 
